022f79a56db8747b9665203af5af045df42912b4
Huynh Tran Phu <huynhtranphu05121999@gmail.com>
2020-12-11 11:49:59 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports={
+  //"facebook_api_key"      :     "414256479200407",
+  "facebook_api_key"      :     "2730852587166024",
+  // "facebook_api_secret"   :     "a73fd736a0ae632724d13b1a2026eb7a",2c6bfabda9437832ca10806eaa2d6118
+  "facebook_api_secret"   :     "2c6bfabda9437832ca10806eaa2d6118",
+  "callback_url"          :     "http://localhost:4000/auth/facebook/callback",
+  "use_database"          :     true,
+  "host"                  :     "localhost",
+  "username"              :     "root",
+  "password"              :     "",
+  "database"              :     "login_passport"
+}
